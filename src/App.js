883b9eaefa020bfa9b015c3ab51c4fc28301a14d
felipediaz3974@gmail.com <felipediaz3974@gmail.com>
2024-07-29 16:08:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/pessoa';
+
 
 function App() {
+  const nome= 'Augusto'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld />
+      <SayMyName nome="Davi"/>
+      <SayMyName nome="Felipe"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Davi" idade="20" profissao="Programador" foto="https://via.placeholder.com/150" />
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
